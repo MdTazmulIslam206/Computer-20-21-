a5c7595aacd9cc70bdbd1c1b40760090f463a91a
MdTazmulIslam206 <142989526+MdTazmulIslam206@users.noreply.github.com>
2024-03-11 00:49:13 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function Sofikul() {
+    alert("Amader department er sobchye valo sele. But aktu ragi.")
+}
+function Tazmul() {
+    alert("Amader department er karap sele . But akon se namaj pore.")
+}
+function Joynto() {
+    alert("Amader department er sobchye chalak sele . But aktu besei soitan.")
+}
+function Rashed() {
+    alert("Amader department er intelign sele . But aktu meye baji.")
+}
+function LD_Lyon() {
+    alert("Amader department er moha pramik . But sele ta valo")
+}
+function Rony() {
+    alert("Amader department er horlix khor sele . But se kaj er dik dye onak agie ase.")
+}
+function Ripon() {
+    alert("Amader department er bes valo sele . But aktu besei chalak.")
+}
+function Fahim() {
+    alert("Amader department er valo sele . But aktu olos .")
+}
+function Kulsum() {
+    alert("Amader department er akjon pothibadi meye . But aktu soitan o.")
+}
+function Popi() {
+    alert("Jar khotha na bolei noi , Amader department er sobchite santo meye . But aktu besei santo , maje mode di thin da gali na dile hoi.")
+}
+function Morium() {
+    alert("Amader department er valo meye .But aktu lajuk.")
+}
+function Simu() {
+    alert("E hoilo simu rani, santo meye . But aktu buje besi.")
+}
+
+
+
+
+
+
